48fd16aa1514d17e3f29dd512a7de7c877628bab
nabe-hayato <nachtschnee001@gmail.com>
2023-09-19 03:26:44 +0900

    v2

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ async function transcribe_witai(buffer) {
 
 // Google Speech API
 // https://cloud.google.com/docs/authentication/production
-const gspeech = require('@google-cloud/speech');
+const gspeech = require('@google-cloud/speech').v2;
 const gspeechclient = new gspeech.SpeechClient({
   projectId: 'discordbot',
   keyFilename: 'gspeech_key.json'
